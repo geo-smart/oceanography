b14adab8f82132645e1e2192cee14314310c0f08
Rob Fatland <rob5@uw.edu>
2024-06-19 15:41:41 -0700

    majone

--- book/chapters/rob/anomaly.py
+++
@@ -1,9 +0,0 @@
-import os, sys, time, glob, warnings
-from matplotlib import pyplot as plt
-from matplotlib import colors as mplcolors
-from matplotlib.pyplot import figure
-import numpy as np, pandas as pd, xarray as xr
-from numpy import datetime64 as dt64, timedelta64 as td64
-from math import fabs, sqrt
-
-
